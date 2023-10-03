df5747e1c9ce81e27594922f88273d7a0cf6ab2c
SalBEH8 <salembehatt@gmail.com>
2023-10-03 22:58:16 +0200

    First commit

---
+++ src/component/createAccount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function createAccount(){
+    return (
+        <div>
+            <p>Creation de compte</p>
+        </div>
+    )
+}
+
+export default createAccount;
---
+++ src/component/logIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function logIn(){
+    return (
+        <div>
+            <p>Log in</p>
+        </div>
+    )
+}
+
+export default logIn;
