3e9cb74ac0eae43902bbd73b0bb785ea69ec48c2
SalBEH8 <salembehatt@gmail.com>
2023-10-03 23:45:28 +0200

    footer first commit

--- src/component/home.js
+++ src/component/home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "../css/home.css";
+import Footer from "./footer"
 
 
 function Home(){
     return (
         <div className="body">
             <h1>Bordeau commu</h1>
+            <Footer />
         </div>
     );
-    
+
 }
 
 export default Home;
