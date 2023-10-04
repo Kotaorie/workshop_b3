7a54b2d63bfd191c58689f460a2a3f3f28b9f65b
SalBEH8 <salembehatt@gmail.com>
2023-10-04 10:38:15 +0200

    salmem

--- src/component/createAccount.js
+++ src/component/createAccount.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "../css/home.css";
+import Footer from "./footer"
 
 function createAccount(){
     return (
         <div>
             <p>Creation de compte</p>
+            <Footer />
         </div>
     )
 }
--- src/component/garden.js
+++ src/component/garden.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Footer from "./footer";
+
 
 function Garden(){
     return (
         <div>
             <p>jardinage</p>
+            <Footer />
         </div>
     );
     
