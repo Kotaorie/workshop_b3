bc1dcccfa07975f515c9a656a2ca06168e65f151
SalBEH8 <salembehatt@gmail.com>
2023-10-04 11:48:15 +0200

    formulaires ok

--- src/component/createAccount.js
+++ src/component/createAccount.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../css/home.css";
-import Footer from "./footer"
 
-function createAccount(){
-    return (
-        <div>
-            <p>Creation de compte</p>
-            <Footer />
-        </div>
-    )
+function CreateAccount(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const handleRegister = () => {}
+return (
+    <div>
+        <h2>Inscription</h2>
+        <form>
+        <fieldset>
+            <div>
+                <label>
+                    Nom d'utilisateur
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}  />
+                </label>
+            </div>
+            <div>
+                <label>
+                    E-mail:
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Télephone
+                    <input type="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                </label>
+            </div>
+            <div>
+            <label>
+              Mot de passe:
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Confirmez le mot de passe:
+              <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+            </label>
+          </div>
+          <p>Déjà inscrit ? Connectez-vous.</p>
+        <button type="button" onClick={handleRegister}>
+          Créer un compte
+        </button>
+        </fieldset>
+        </form>
+    </div>
+    );
 }
 
-export default createAccount;
+export default CreateAccount;
--- src/component/logIn.js
+++ src/component/logIn.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../css/home.css";
 
-function logIn(){
+
+function LogIn(){
+    const [email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+    const handleLogin = () => {}
     return (
         <div>
-            <p>Log in</p>
+            <h2>Connexion</h2>
+            <form>
+            <fieldset>
+                <div>
+                <label>
+                    E-mail
+                    <input type="email" v alue={email} onChange={(e) =>setEmail(e.target.value)} />
+                </label>
+                </div>
+                <div>
+                <label>
+                    Mot de passe type
+                    <input type="password" value={password} onChange={(e) =>setPassword(e.target.value)} />
+                </label>
+                </div>
+                <p>Mot de passe oublié ?</p>
+                <button type="button" onClick={handleLogin}>
+                    Connexion
+                </button>
+                <p>Pas encore de compte ? Inscrivez-vous</p>
+            </fieldset>
+            </form>
         </div>
     )
 }
 
-export default logIn;
+export default LogIn;
