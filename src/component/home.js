f2b60fe8f04b02b82b1651496f671a65ce5cabcc
junior Bruce <madjrijosephjunior@gmail.com>
2023-10-04 12:54:52 +0200

    Page d'accueil provisoire

--- src/component/home.js
+++ src/component/home.js
@@ -1,13 +1,73 @@
-import React from "react";
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React , { useEffect, useState } from "react";
 import "../css/home.css";
+import "../css/caroussel.css";
 import Footer from "./footer"
 
-
 function Co_missions(){
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const images = [
+      "/img/honey-352205_1280.jpg",
+      "/img/nature-845849_1280.jpg",
+      "/img/windmill-5591464_1280.jpg",
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          nextSlide();
+        }, 10000); // Changez cette valeur pour ajuster la durée entre les diapositives (en millisecondes)
+    
+        return () => {
+          clearInterval(interval);
+        };
+      }, []);
+    
+      const previousSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide === 0 ? images.length - 1 : prevSlide - 1));
+      };
+    
+      const nextSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide === images.length - 1 ? 0 : prevSlide + 1));
+      };
+    
     return (
         <div className="body">
-            <div><h1>Titre</h1></div>
-            <h2>test composent</h2>
+            <div>
+                <h1>Bienvenue sur CO'PART</h1>
+            </div>
+
+            <div className="carousel">
+                <div className="carousel-buttons prev-button">
+                    <button className="prev" onClick={previousSlide}>
+                        &#9664; 
+                    </button>
+                </div>
+
+
+                    <img className={`carousel-slide ${currentSlide === 0 ? "active" : ""}`}
+                        src={images[currentSlide]}
+                        alt={`image${currentSlide + 1}`}
+                    />
+
+
+                <div className="carousel-buttons next-button">
+                    <button className="next" onClick={nextSlide}>
+                        &#9654;
+                    </button>
+                </div>
+            </div>
+
+            <div className="homeSpace">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ducimus qui natus velit officiis totam obcaecati itaque veritatis accusantium ad eveniet tempore laudantium, nihil in sit earum voluptatum fugit. Ab!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt accusamus placeat beatae aspernatur iure consectetur eaque minima eligendi! Harum natus, aut non debitis in quisquam soluta magni ut ipsum. Nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam placeat, eligendi atque maxime accusamus tenetur cumque distinctio veniam quos tempora aliquid, fugit dolorum optio earum, nihil explicabo esse delectus?
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt accusamus placeat beatae aspernatur iure consectetur eaque minima eligendi! Harum natus, aut non debitis in quisquam soluta magni ut ipsum. Nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam placeat, eligendi atque maxime accusamus tenetur cumque distinctio veniam quos tempora aliquid, fugit dolorum optio earum, nihil explicabo esse delectus?
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt accusamus placeat beatae aspernatur iure consectetur eaque minima eligendi! Harum natus, aut non debitis in quisquam soluta magni ut ipsum. Nesciunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aperiam placeat, eligendi atque maxime accusamus tenetur cumque distinctio veniam quos tempora aliquid, fugit dolorum optio earum, nihil explicabo esse delectus?
+                </p>
+
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit officia, nulla voluptatum quam voluptate repellat saepe? Aspernatur, officia facere animi veritatis molestias ullam. Repellat sint at provident a quia numquam.</p>
+            </div>
+                
             <Footer />
         </div>
     );
