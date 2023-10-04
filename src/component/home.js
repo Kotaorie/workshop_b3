572213c62b17ed9649aeb917beb2764d4030d935
nico servary <nicolas.servary@epsi.fr>
2023-10-04 08:36:48 +0200

    merge des 2 push precedents

--- src/component/home.js
+++ src/component/home.js
@@ -6,13 +6,9 @@ import Footer from "./footer"
 function Co_missions(){
     return (
         <div className="body">
-<<<<<<< HEAD
             <div><h1>Titre</h1></div>
             <h2>test composent</h2>
-=======
-            <h1>Bordeau commu</h1>
             <Footer />
->>>>>>> 3e9cb74ac0eae43902bbd73b0bb785ea69ec48c2
         </div>
     );
 
