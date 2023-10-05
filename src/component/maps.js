d11c5744f0c1e5d542b74087695c216a3cf88211
SalBEH8 <salembehatt@gmail.com>
2023-10-05 23:25:05 +0200

    Maj google maps plus sympa

--- src/component/maps.js
+++ src/component/maps.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import '../css/mapStyles.css';
 
 const MapContainer = () => {
   const mapStyles = {
@@ -12,23 +13,60 @@ const MapContainer = () => {
     lng: -0.579180,
   };
 
+  const [activeMarker, setActiveMarker] = useState(null);
+  const [activeRow, setActiveRow] = useState(null);
+
   const gardenLocations = [
-    { lat: 44.837789, lng: -0.579180, name: 'Bordeaux' },
-    { lat: 44.837789, lng: -0.580000, name: 'Jardin Public 1' },
-    { lat: 44.830000, lng: -0.590000, name: 'Jardin Public 2' },
-    { lat: 44.840000, lng: -0.570000, name: 'Jardin Public 3' },
-    { lat: 44.845000, lng: -0.585000, name: 'Jardin Public 4' },
-    { lat: 44.835000, lng: -0.575000, name: 'Jardin Public 5' },
+    { lat: 44.849729, lng: -0.57993, name: 'Jardin Botanique du Jardin Public', contact: 'Tel : 0556521877, Jardin Botanique du Jardin Public' },
+    { lat: 44.864663, lng: -0.549492, name: 'LartPlace Victor Raulin', contact: 'Association les Vivres de lart' },
+    { lat: 44.871709, lng: -0.569116, name: 'Ferme pédagogique Jardin partagé', contact: 'Ferme pédagogique de Bordeaux Lac, Email : ca.lelac@centres-animation.asso.fr' },
+    { lat: 44.875098, lng: -0.544152, name: 'Bacalan Jardins familiaux Parc de Bacalan', contact: 'Association Bocal Local, Email : jardins-familiaux@lebocallocal.fr' },
   ];
 
+  const handleMarkerClick = (index) => {
+    setActiveMarker(index);
+    setActiveRow(null); // Réinitialise la ligne active lors du clic sur un marqueur
+  };
+
+  const handleRowClick = (index) => {
+    setActiveMarker(null); // Réinitialise le marqueur actif lors du clic sur une ligne
+    setActiveRow(index);
+  };
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyAJkGX1bC9SaWnFGLZx6DAAwZvcrePHLBg" libraries={['places']}>
-      <GoogleMap mapContainerStyle={mapStyles} zoom={12} center={defaultCenter}>
-        {gardenLocations.map((location, index) => (
-          <Marker key={index} position={{ lat: location.lat, lng: location.lng }} title={location.name} />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+    <div id="map-container">
+      <LoadScript googleMapsApiKey="AIzaSyAJkGX1bC9SaWnFGLZx6DAAwZvcrePHLBg" libraries={['places']}>
+        <GoogleMap mapContainerStyle={mapStyles} zoom={12} center={defaultCenter}>
+          {gardenLocations.map((location, index) => (
+            <Marker
+              key={index}
+              position={{ lat: location.lat, lng: location.lng }}
+              title={location.name}
+              onClick={() => handleMarkerClick(index)}
+              animation={activeMarker === index ? 1 : 0}
+            />
+          ))}
+        </GoogleMap>
+      </LoadScript>
+
+      <h2>Liste des Jardins :</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nom du Jardin</th>
+            <th>Contact</th> {}
+          </tr>
+        </thead>
+        <tbody>
+          {gardenLocations.map((location, index) => (
+            <tr key={index} className={activeRow === index ? 'active' : ''} onClick={() => handleRowClick(index)}>
+              <td>{location.name}</td>
+              <td>{activeRow === index ? location.contact : ''}</td> {/* Affichez le contact si la ligne est active */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
